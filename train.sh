0e3022d9212bb2d8c88bb2a86ad5422e183ef5dc
Taku Araki <tenka8624@gmail.com>
2025-09-18 00:51:45 +0900

    fix: Changed Capacity Loss gate from -200.0 to -120.0 feat: I forgat to commit changes which appied hypersphere constraint The score suddenly went down amdist the training. I think it's because the capacity loss went down the way too faster.

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 env_name=MsPacman
 seed=1
 python -u train.py \
-    -n "v1.0.0-${env_name}-life_done-wm_2L512D8H-100k-seed${seed}" \
+    -n "v1.1.1-hyperSphere-${env_name}-life_done-wm_2L512D8H-100k-seed${seed}" \
     -seed ${seed} \
     -config_path "config_files/STORM.yaml" \
     -env_name "ALE/${env_name}-v5" \
     -trajectory_path "D_TRAJ/${env_name}.pkl" \
-    -record_run \
+    -record_run 
